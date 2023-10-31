9bea190fdfa4c99478cff8582ba05784c8e8c1f8
DoublEffe <fabiofagiani9@gmail.com>
2023-10-31 11:22:07 +0100

    bo

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function handler(mediaQuery,popoverList){
   if (mediaQuery.matches){
     console.log('poi')
+    popoverList.map((popover) => popover.dispose())
     let span = document.querySelector('.trunc');
     span.removeAttribute('role')
     span.removeAttribute('data-bs-toggle')
@@ -16,10 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
   let popoverList = popoverTriggerList.map( (popoverTriggerEl) =>
    new bootstrap.Popover(popoverTriggerEl)
 )
-  let mediaQuery = window.matchMedia('(min-width:426px)')
+  //let mediaQuery = window.matchMedia('(min-width:426px)')
 
-  console.log(mediaQuery)
-  mediaQuery.addEventListener('change',handler)
-  handler(mediaQuery,popoverList)
+  //console.log(mediaQuery)
+  //mediaQuery.addEventListener('change',handler)
+  //handler(mediaQuery,popoverList)
   
 })
